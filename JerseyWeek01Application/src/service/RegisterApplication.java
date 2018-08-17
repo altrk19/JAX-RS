8dd230468bb7bff1b76e0d0a5642105615713243
altrk19 <altrk19@gmail.com>
2018-08-17 18:16:23 +0300

    JerseyWeek01Application

---
+++ JerseyWeek01Application/src/service/RegisterApplication.java
@@ -0,0 +1,23 @@
+package service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import controller.MessageService;
+
+public class RegisterApplication extends Application{
+
+	private Set<Object> singletons = new HashSet<Object>();
+	
+	public RegisterApplication () {
+		singletons.add(new MessageService());
+	}
+	
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
+// rest service siniflari ,. singleton ya da per request olabilir.
